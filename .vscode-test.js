f8114d83476ae2c92a50043278af7042e0803e61
yugook <150545429+yugook@users.noreply.github.com>
2025-09-30 22:56:43 +0900

    DBus のセッションバスアドレスを修正し、mocha のタイムアウト設定を追加

--- .vscode-test.js
+++ .vscode-test.js
@@ -4,7 +4,11 @@ module.exports = defineConfig({
     files: 'out/test/extension.test.js',
     extensionDevelopmentPath: __dirname,
     launchArgs: ['--disable-gpu', '--disable-features=CalculateNativeWinOcclusion'],
+    mocha: {
+        timeout: 20000,
+    },
     env: {
-        DBUS_SESSION_BUS_ADDRESS: '/dev/null',
+        DBUS_SESSION_BUS_ADDRESS: 'unix:path=/dev/null',
+        NO_AT_BRIDGE: '1',
     },
 });
