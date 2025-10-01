29e3b47e9ba65aee65b8b7943261f411352681da
yugook <150545429+yugook@users.noreply.github.com>
2025-10-01 23:06:49 +0900

    修正

--- .vscode-test.js
+++ .vscode-test.js
@@ -1,8 +1,15 @@
 const { defineConfig } = require('@vscode/test-cli');
+const fs = require('fs');
+const path = require('path');
+
+const workspaceDir = path.join(__dirname, 'tmp', 'test-workspace');
+fs.mkdirSync(workspaceDir, { recursive: true });
+
 module.exports = defineConfig({
     files: 'out/test/extension.test.js',
     extensionDevelopmentPath: __dirname,
     launchArgs: [
+        workspaceDir,
         '--disable-gpu',
         '--disable-features=CalculateNativeWinOcclusion',
         '--disable-dev-shm-usage',
