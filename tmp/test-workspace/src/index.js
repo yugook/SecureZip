eafcae23babeb1ea8ddace705fa3c2f664c9d225
yugook <150545429+yugook@users.noreply.github.com>
2025-10-01 23:17:43 +0900

    追加の修正

---
+++ tmp/test-workspace/src/index.js
@@ -0,0 +1,10 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+exports.greet = greet;
+function greet(name) {
+    return `Hello, ${name}!`;
+}
+if (require.main === module) {
+    console.log(greet('SecureZip'));
+}
+//# sourceMappingURL=index.js.map
