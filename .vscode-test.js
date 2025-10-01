43f6e9fbbf5a310270de9f8dee56954346643e21
yugook <150545429+yugook@users.noreply.github.com>
2025-10-01 22:11:51 +0900

    ログを出力するように変更

--- .vscode-test.js
+++ .vscode-test.js
@@ -3,7 +3,12 @@ const { defineConfig } = require('@vscode/test-cli');
 module.exports = defineConfig({
     files: 'out/test/extension.test.js',
     extensionDevelopmentPath: __dirname,
-    launchArgs: ['--disable-gpu', '--disable-features=CalculateNativeWinOcclusion'],
+    launchArgs: [
+        '--disable-gpu',
+        '--disable-features=CalculateNativeWinOcclusion',
+        '--disable-dev-shm-usage',
+        '--verbose',
+    ],
     mocha: {
         timeout: 20000,
     },
